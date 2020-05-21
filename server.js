0bd743c6d48f475a2bea8851783daac88ee3944a
Michal Kaštan <41683416+BloodyBogan@users.noreply.github.com>
2020-05-21 21:10:05 +0200

    Fix

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config({ path: '.env' });
+  require('dotenv').config();
 }
 
 const express = require('express');
@@ -15,9 +15,9 @@ app.use(expressLayouts);
 app.use(express.static('public'));
 
 const mongoose = require('mongoose');
-mongoose.connect(process.env.DATABASE_URL, {
-  useNewUrlParser: true,
+mongoose.set('useCreateIndex', true).connect(process.env.DATABASE_URL, {
   useUnifiedTopology: true,
+  useNewUrlParser: true,
 });
 const db = mongoose.connection;
 db.on('error', (error) => console.error());
